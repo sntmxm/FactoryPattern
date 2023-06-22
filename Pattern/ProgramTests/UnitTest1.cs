d9bd6cd6df31db4f3631043c2a89f2d13d255102
Max Schastnyi <maksch-ipt25@lll.kpi.ua>
2023-06-22 20:45:10 +0300

    Create NUnit test (ProgramTests)

---
+++ Pattern/ProgramTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ProgramTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
