9c46e09db3b4966e0f3d4b5df64e0465f86b2a1a
Max Schastnyi <maksch-ipt25@lll.kpi.ua>
2023-06-22 21:37:51 +0300

    Make four tests

--- Pattern/ProgramTests/UnitTest1.cs
+++ Pattern/ProgramTests/UnitTest1.cs
@@ -1,16 +1,72 @@
+using Pattern;
+
 namespace ProgramTests
 {
-    public class Tests
+    [TestFixture]
+    public class CarFactoryTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void SedanFactoryCreateSedanType()
         {
+            ICarFactory sedanFactory = new SedanFactory();
+            IReleaseNewModel sedan = sedanFactory.Create();
+            Assert.IsInstanceOf<Sedan>(sedan);
         }
 
         [Test]
-        public void Test1()
+        public void CoupeFactoryCreateCoupeType()
         {
-            Assert.Pass();
+            ICarFactory coupeFactory = new CoupeFactory();
+            IReleaseNewModel coupe = coupeFactory.Create();
+            Assert.IsInstanceOf<Coupe>(coupe);
+        }
+
+        public class ConsoleCapture : IDisposable
+        {
+            private StringWriter stringWriter;
+            private TextWriter originalOutput;
+
+            public ConsoleCapture()
+            {
+                stringWriter = new StringWriter();
+                originalOutput = Console.Out;
+                Console.SetOut(stringWriter);
+            }
+
+            public string Output => stringWriter.ToString();
+
+            public void Dispose()
+            {
+                Console.SetOut(originalOutput);
+                stringWriter.Dispose();
+            }
+        }
+
+        [Test]
+        public void SedanRelease()
+        {
+
+            IReleaseNewModel sedan = new Sedan();
+            using (var consoleOutput = new ConsoleCapture())
+            {
+                sedan.Release();
+                string output = consoleOutput.Output;
+                StringAssert.Contains("Released sedan", output);
+            }
+        }
+
+        [Test]
+        public void CoupeRelease()
+        {
+            IReleaseNewModel coupe = new Coupe();
+            using (var consoleOutput = new ConsoleCapture())
+            {
+                coupe.Release();
+                string output = consoleOutput.Output;
+                StringAssert.Contains("Released coupe", output);
+            }
         }
     }
+
+
 }
