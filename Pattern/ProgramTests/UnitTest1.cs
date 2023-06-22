f644fff2ace0f34454a1a0c74de769a12c6ee84b
Max Schastnyi <maksch-ipt25@lll.kpi.ua>
2023-06-22 22:33:10 +0300

    Correction of excess

--- Pattern/ProgramTests/UnitTest1.cs
+++ Pattern/ProgramTests/UnitTest1.cs
@@ -21,51 +21,6 @@ namespace ProgramTests
             Assert.IsInstanceOf<Coupe>(coupe);
         }
 
-        public class ConsoleCapture : IDisposable
-        {
-            private StringWriter stringWriter;
-            private TextWriter originalOutput;
-
-            public ConsoleCapture()
-            {
-                stringWriter = new StringWriter();
-                originalOutput = Console.Out;
-                Console.SetOut(stringWriter);
-            }
-
-            public string Output => stringWriter.ToString();
-
-            public void Dispose()
-            {
-                Console.SetOut(originalOutput);
-                stringWriter.Dispose();
-            }
-        }
-
-        [Test]
-        public void SedanRelease()
-        {
-
-            IReleaseNewModel sedan = new Sedan();
-            using (var consoleOutput = new ConsoleCapture())
-            {
-                sedan.Release();
-                string output = consoleOutput.Output;
-                StringAssert.Contains("Released sedan", output);
-            }
-        }
-
-        [Test]
-        public void CoupeRelease()
-        {
-            IReleaseNewModel coupe = new Coupe();
-            using (var consoleOutput = new ConsoleCapture())
-            {
-                coupe.Release();
-                string output = consoleOutput.Output;
-                StringAssert.Contains("Released coupe", output);
-            }
-        }
     }
 
 
